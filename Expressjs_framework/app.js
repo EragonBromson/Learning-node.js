615820a96882204fea4ddec082cd01db98b56759
Sarthak Agrawal <sarthak.agrawal76@gmail.com>
2016-03-15 04:51:31 +0530

    Handling routes

--- Expressjs_framework/app.js
+++ Expressjs_framework/app.js
@@ -4,14 +4,19 @@ var app = express();
 
 app.get('/', function(request, response) {
   response.setHeader('Content-Type', 'text/plain');
-  response.write("You are on the root page. :P");
+  response.write("You are on the root page.");
   response.end();
+});
+
+app.get('/floor/:floornum/bedroom', function(request, response) {
+    response.setHeader('Content-Type', 'text/plain');
+    response.end('You are in the bedroom on floor number ' + request.params.floornum);
 });
 
 //FOR ALL THE PAGES THAT DONT EXIST THIS PIECE OF CODE IS REQUIRED
 app.use(function(request, response, next){
   response.setHeader('Content-Type', 'text/plain');
-  response.send(400,"Page not found.");
+  response.send(404,"Page not found.");
 });
 
 app.listen(8080);
