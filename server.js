42b966d27356ac73c20d64e6ed914a0d9791636f
Sarthak Agrawal <sarthak.agrawal76@gmail.com>
2016-03-14 19:28:40 +0530

    parameters in url

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 var http = require('http');
 var url = require('url');
+var querystring = require('querystring');
 
 var server = http.createServer(function(request,response) {
 
     var page = url.parse(request.url).pathname;
     console.log(page);
+    var parameters = querystring.parse(url.parse(request.url).query);
 
     // response.writeHead(200, {"Content-Type" : "text/html"});  //Status code in header and MIME type
     if(page == '/'){
       response.writeHead(200, {"Content-Type" : "text/html"});
-      response.write("Hello this is the text.\n <p>Due to Content-Type now, I am able to show this <strong>HTML</strong> text. </p>");
+      response.write("Hello this is the text.\n"+
+      "<p>Due to Content-Type now, I am able to show this <strong>HTML</strong> text. </p>");
     }
     else if (page == '/mypage') {
       response.writeHead(200, {"Content-Type" : "text/html"});
@@ -18,6 +21,11 @@ var server = http.createServer(function(request,response) {
     else {
       response.writeHead(404);  //No Content-Type with error code
       response.write("<p> <strong>Error</strong> </p>");
+    }
+    if('firstname' in parameters && 'secondname' in parameters && response.statusCode != '404'){
+      response.write( "parameters are : \nfirstname = " + parameters['firstname']+
+      "\nsecondname = " + parameters['secondname'] + "\n" );
+
     }
     response.end("This is the end.");
   });
