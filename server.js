64cd1f35446d9379264d19f2f567ebd292ec016b
Sarthak Agrawal <sarthak.agrawal76@gmail.com>
2016-03-14 19:01:25 +0530

    Different pages and error code

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 var http = require('http');
+var url = require('url');
 
 var server = http.createServer(function(request,response) {
-    response.writeHead(200, {"Content-Type" : "text/html"});  //Status code in header and MIME type
-    response.write("Hello this is the text.\n <p>Due to Content-Type now, I am able to show this <strong>HTML</strong> text. </p>");
+
+    var page = url.parse(request.url).pathname;
+    console.log(page);
+
+    // response.writeHead(200, {"Content-Type" : "text/html"});  //Status code in header and MIME type
+    if(page == '/'){
+      response.writeHead(200, {"Content-Type" : "text/html"});
+      response.write("Hello this is the text.\n <p>Due to Content-Type now, I am able to show this <strong>HTML</strong> text. </p>");
+    }
+    else if (page == '/mypage') {
+      response.writeHead(200, {"Content-Type" : "text/html"});
+      response.write("<p> This is my page. </p>");
+    }
+    else {
+      response.writeHead(404);  //No Content-Type with error code
+      response.write("<p> <strong>Error</strong> </p>");
+    }
     response.end("This is the end.");
   });
 server.listen(8080);  //listen to connection on this port
